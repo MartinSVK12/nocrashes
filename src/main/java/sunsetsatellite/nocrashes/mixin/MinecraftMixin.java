6d890ee81385eac42831d3e9d3208aa8254cd489
MartinSVK12 <martinmilesich2882@gmail.com>
2023-03-12 00:06:53 +0100

    Updated to BTA 1.7.7.0 pre-release 1.

--- src/main/java/sunsetsatellite/nocrashes/mixin/MinecraftMixin.java
+++ src/main/java/sunsetsatellite/nocrashes/mixin/MinecraftMixin.java
@@ -1,18 +1,37 @@
 package sunsetsatellite.nocrashes.mixin;
 
-import bta.ModLoader;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.MinecraftApplet;
-import net.minecraft.src.*;
-import net.minecraft.src.command.ClientCommandHandler;
-import net.minecraft.src.helper.Time;
-import net.minecraft.src.input.MouseInput;
-import net.minecraft.src.input.controller.ControllerInput;
-import net.minecraft.src.render.IRenderer;
+import net.minecraft.client.render.camera.ICamera;
+import net.minecraft.core.*;
+import net.minecraft.core.achievement.stat.StatFileWriter;
+import net.minecraft.core.entity.EntityLiving;
+import net.minecraft.core.entity.player.EntityPlayer;
+import net.minecraft.core.entity.player.EntityPlayerSP;
+import net.minecraft.core.enums.EnumOS2;
+import net.minecraft.core.gui.*;
+import net.minecraft.core.input.InputType;
+import net.minecraft.core.input.MouseInput;
+import net.minecraft.core.input.controller.ControllerInput;
+import net.minecraft.core.net.command.ClientCommandHandler;
+import net.minecraft.core.net.handler.NetClientHandler;
+import net.minecraft.core.net.thread.ThreadDownloadResources;
+import net.minecraft.core.option.GameSettings;
+import net.minecraft.core.player.Session;
+import net.minecraft.core.player.controller.PlayerController;
+import net.minecraft.core.render.*;
+import net.minecraft.core.sound.SoundManager;
+import net.minecraft.core.util.helper.Time;
+import net.minecraft.core.util.phys.AABB;
+import net.minecraft.core.util.phys.Vec3d;
+import net.minecraft.core.world.World;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
-import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import sunsetsatellite.nocrashes.GuiException;
 
 import java.awt.*;
@@ -22,65 +41,74 @@ import java.awt.*;
         remap = false
 )
 public abstract class MinecraftMixin {
+
+    @Shadow public volatile boolean running;
+
+    @Shadow public abstract void startGame() throws LWJGLException;
+
+    @Shadow public abstract void onMinecraftCrash(UnexpectedThrowable unexpectedthrowable);
+
+    @Shadow protected MinecraftApplet mcApplet;
+
+    @Shadow public Canvas mcCanvas;
+
+    @Shadow public abstract void shutdown();
+
+    @Shadow public volatile boolean isGamePaused;
+
+    @Shadow public World theWorld;
+
+    @Shadow private Timer timer;
+
+    @Shadow @Final public MouseInput mouseInput;
+
+    @Shadow public ControllerInput controllerInput;
+
+    @Shadow private int ticksRan;
+
+    @Shadow public abstract void runTick();
+
+    @Shadow public abstract void changeWorld1(World world);
+
+    @Shadow public abstract void displayGuiScreen(GuiScreen guiscreen);
+
     @Shadow public PlayerController playerController;
     @Shadow private boolean fullscreen;
-    @Shadow private Timer timer;
-    @Shadow public World theWorld;
     @Shadow public EntityPlayerSP thePlayer;
-    @Shadow public Canvas mcCanvas;
-    @Shadow public volatile boolean isGamePaused;
     @Shadow public GuiScreen currentScreen;
-    @Shadow public EntityRenderer entityRenderer;
-    @Shadow private int ticksRan;
+    @Shadow public WorldRenderer worldRenderer;
     @Shadow public GuiAchievement guiAchievement;
     @Shadow public boolean skipRenderWorld;
     @Shadow public GameSettings gameSettings;
-    @Shadow protected MinecraftApplet mcApplet;
     @Shadow public SoundManager sndManager;
-    @Shadow public volatile boolean running;
     @Shadow public String debug;
     @Shadow public String debugFPS;
     @Shadow long prevFrameTime;
     @Shadow @Final public GameResolution resolution;
-    @Shadow @Final public ClientCommandHandler commandHandler;
     @Shadow public IRenderer render;
-    @Shadow public ControllerInput controllerInput;
-    @Shadow public MouseInput mouseInput;
 
-    @Shadow public abstract void onMinecraftCrash(UnexpectedThrowable unexpectedthrowable);
-    @Shadow public abstract void startGame() throws LWJGLException;
-
-    @Shadow public abstract void displayGuiScreen(GuiScreen guiscreen);
-
-    @Shadow protected abstract void checkGLError(String s);
+    @Shadow protected abstract void checkGLError(String stageName);
 
     @Shadow public abstract void shutdownMinecraftApplet();
 
-    @Shadow public abstract void func_28002_e();
+    @Shadow public abstract void freeUpMemory();
 
     @Shadow protected abstract void screenshotListener();
 
-    @Shadow protected abstract void displayDebugInfo(long l);
-
-    @Shadow public abstract void shutdown();
+    @Shadow protected abstract void drawFrameTimeGraph(long frameTime);
 
     @Shadow public abstract boolean shadersActive();
 
     @Shadow public abstract void displayInGameMenu();
 
-    @Shadow public abstract void toggleFullscreen();
-
     @Shadow protected abstract void resize();
 
     @Shadow public abstract boolean isMultiplayerWorld();
 
-    @Shadow public abstract void changeWorld1(World world);
-
-    @Shadow public abstract void runTick();
 
     /**
      * @author MartinSVK12
-     * @reason Better crash handler
+     * @reason Better crash handler.
      */
     @Overwrite
     public void run() {
@@ -95,8 +123,8 @@ public abstract class MinecraftMixin {
         }
 
         try {
-            long l = System.currentTimeMillis();
-            int i = 0;
+            long currentTime = System.currentTimeMillis();
+            int fpsCounter = 0;
 
             while(this.running) {
                 try {
@@ -106,46 +134,42 @@ public abstract class MinecraftMixin {
                         break;
                     }
 
-                    AxisAlignedBB.clearBoundingBoxPool();
-                    Vec3D.initialize();
+                    AABB.initializePool();
+                    Vec3d.initializePool();
                     if (this.mcCanvas == null && Display.isCloseRequested()) {
                         this.shutdown();
                     }
 
-                    if (Display.isFullscreen() && !Display.isActive()) {
-                        this.toggleFullscreen();
-                    }
-
                     if (this.isGamePaused && this.theWorld != null) {
-                        float f = this.timer.renderPartialTicks;
+                        float temp = this.timer.renderPartialTicks;
                         this.timer.updateTimer();
-                        this.timer.renderPartialTicks = f;
+                        this.timer.renderPartialTicks = temp;
                     } else {
                         this.timer.updateTimer();
                     }
 
-                    long l1 = System.nanoTime();
+                    long nanoTime = System.nanoTime();
                     this.mouseInput.update();
                     if (this.controllerInput != null && this.currentScreen != null) {
                         this.controllerInput.moveCursor();
                     }
 
-                    for(int j = 0; j < this.timer.elapsedTicks; ++j) {
+                    for(int tick = 0; tick < this.timer.elapsedTicks; ++tick) {
                         ++this.ticksRan;
 
                         try {
                             this.runTick();
                         } catch (MinecraftException var16) {
                             this.theWorld = null;
-                            this.changeWorld1(null);
+                            this.changeWorld1((World)null);
                             this.displayGuiScreen(new GuiConflictWarning());
                         }
                     }
 
-                    long l2 = System.nanoTime() - l1;
+                    long frameTime = System.nanoTime() - nanoTime;
                     this.checkGLError("Pre render");
-                    RenderBlocks.fancyGrass = this.gameSettings.fancyGraphics.value;
-                    this.sndManager.func_338_a(this.thePlayer, this.timer.renderPartialTicks);
+                    RenderBlocks.fancyGrass = (Integer)this.gameSettings.fancyGraphics.value == 1;
+                    this.sndManager.updateListenerPosition(this.thePlayer, this.timer.renderPartialTicks);
                     GL11.glEnable(3553);
                     if (this.theWorld != null) {
                         this.theWorld.updatingLighting();
@@ -166,15 +190,16 @@ public abstract class MinecraftMixin {
                             this.playerController.setPartialTime(this.timer.renderPartialTicks);
                         }
 
-                        this.entityRenderer.updateCameraAndRender(this.timer.renderPartialTicks);
+                        this.worldRenderer.updateCameraAndRender(this.timer.renderPartialTicks);
                     }
 
-                    if (this.gameSettings.pauseOnLostFocus.value && !Display.isActive() && this.currentScreen == null) {
+                    if ((Boolean)this.gameSettings.pauseOnLostFocus.value && !Display.isActive() && this.currentScreen == null) {
                         this.displayInGameMenu();
                     }
 
-                    if (this.gameSettings.showFrameTimes.value) {
-                        this.displayDebugInfo(l2);
+                    this.sndManager.setMuted((Boolean)this.gameSettings.muteOnLostFocus.value && !Display.isActive());
+                    if ((Boolean)this.gameSettings.showFrameTimes.value) {
+                        this.drawFrameTimeGraph(frameTime);
                     } else {
                         this.prevFrameTime = System.nanoTime();
                     }
@@ -191,20 +216,20 @@ public abstract class MinecraftMixin {
                     }
 
                     this.checkGLError("Post render");
-                    ++i;
+                    ++fpsCounter;
 
-                    for(this.isGamePaused = !this.isMultiplayerWorld() && this.currentScreen != null && this.currentScreen.doesGuiPauseGame(); System.currentTimeMillis() >= l + 1000L; i = 0) {
-                        this.debug = i + " fps, " + WorldRenderer.chunksUpdated + " chunk updates";
-                        this.debugFPS = i + " fps";
-                        WorldRenderer.chunksUpdated = 0;
-                        l += 1000L;
+                    for(this.isGamePaused = !this.isMultiplayerWorld() && this.currentScreen != null && this.currentScreen.doesGuiPauseGame(); System.currentTimeMillis() >= currentTime + 1000L; fpsCounter = 0) {
+                        this.debug = fpsCounter + " fps, " + ChunkRenderer.chunksUpdated + " chunk updates";
+                        this.debugFPS = fpsCounter + " fps";
+                        ChunkRenderer.chunksUpdated = 0;
+                        currentTime += 1000L;
                     }
                 } catch (MinecraftException var18) {
                     this.theWorld = null;
-                    this.changeWorld1(null);
+                    this.changeWorld1((World)null);
                     this.displayGuiScreen(new GuiConflictWarning());
                 } catch (OutOfMemoryError var19) {
-                    this.func_28002_e();
+                    this.freeUpMemory();
                     this.displayGuiScreen(new GuiErrorScreen());
                     System.gc();
                 } catch (RuntimeException e){
@@ -215,7 +240,7 @@ public abstract class MinecraftMixin {
             }
         } catch (MinecraftError var20) {
         } catch (Throwable var21) {
-            this.func_28002_e();
+            this.freeUpMemory();
             var21.printStackTrace();
             this.onMinecraftCrash(new UnexpectedThrowable("Unexpected error", var21));
         } finally {
@@ -224,3 +249,7 @@ public abstract class MinecraftMixin {
 
     }
 }
+
+/*
+
+ */
